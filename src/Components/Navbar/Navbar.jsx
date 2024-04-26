4567ddec995590a93af46277f5c11b81c246d487
boniaminshawon <shawonboni29@gmail.com>
2024-04-26 23:47:50 +0600

    created the error page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -13,8 +13,8 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li> <NavLink to='/' className={({ isActive }) => isActive ? 'rounded bg-[#b18b5e] font-bold  border' : ' font-semibold'}>
-                                <button className="px-2  py-1 btn-ghost rounded  ">Home</button> </NavLink></li>
+                            <li> <NavLink to='/' className={({ isActive }) => isActive ? 'rounded bg-[#b18b5e] text-lg font-bold  ' : 'text-black font-semibold'}>
+                                <button className="px-2  py-1 text-lg  rounded  ">Home</button> </NavLink></li>
 
                         </ul>
                     </div>
@@ -22,8 +22,8 @@ const Navbar = () => {
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li> <NavLink to='/' className={({ isActive }) => isActive ? 'rounded bg-[#b18b5e] font-bold  border' : ' font-semibold'}>
-                            <button className="px-2  py-1 btn-ghost rounded  ">Home</button> </NavLink></li>
+                        <li> <NavLink to='/' className={({ isActive }) => isActive ? 'rounded bg-[#b18b5e]  font-bold' : 'text-black border border-[#b18b5e] font-semibold'}>
+                            <button className="px-1 text-lg  rounded  ">Home</button> </NavLink></li>
 
                     </ul>
                 </div>
@@ -34,7 +34,7 @@ const Navbar = () => {
                     {user ?
 
                         <div className="">
-                            {/* for mobile */}
+                           
                             <div className="dropdown dropdown-bottom dropdown-hover dropdown-end text-black z-10">
 
                                 <div tabIndex={0} role="button" className="w-12 tooltip   tooltip-info tooltip-left z-10 " data-tip={user.displayName || 'User Name not found'}>
@@ -42,36 +42,24 @@ const Navbar = () => {
                                 </div>
 
 
-                                <ul tabIndex={0} className="dropdown-content text-base font-bold text-white bg-[#00aeff] z-[1] menu  shadow rounded w-[200px]">
+                                <ul tabIndex={0} className="dropdown-content text-base font-bold text-white bg-[#b18b5e] z-[1] menu  shadow rounded w-[200px]">
 
-                                    <li className="hover:bg-[#004274]"><Link to={'/profile'}>Update Profile</Link></li>
+
                                     <hr />
-                                    <li> <button onClick={logOut} className="sm:py-2 py-1 px-3 sm:h-[44px] rounded  bg-[#00aeff] border-white hover:bg-[#004274] sm:text-lg ">Log Out</button></li>
+                                    <li> <button onClick={logOut} className="sm:py-2 py-1 px-3 sm:h-[44px] rounded  bg-[#b18b5e] border-white hover:bg-[#004274] sm:text-lg ">Log Out</button></li>
                                     <hr />
                                 </ul>
                             </div>
 
-                            {/* for tablet and pc */}
-                            {/* <div className="hidden  gap-2 md:flex">
-                                <div className="w-12 tooltip   tooltip-info tooltip-bottom " data-tip={user.displayName || 'User Name not found'}>
-                                    <img className="rounded-full h-[44px] w-[44px] bg-white" alt="" src={user?.photoURL || "https://i.ibb.co/L1kVMdW/images-removebg-preview.png"} />
-                                </div>
-
-
-                                <div>
-                                    <button onClick={logOut} className="sm:py-2 py-1 px-3 sm:h-[44px] rounded font-bold bg-[#00aeff] border-[#00aeff]  text-base sm:text-lg text-white">Log Out</button>
-                                </div>
-                            </div> */}
-
                         </div>
                         :
-                        <Link to={'/login'}> <button className="py-1 sm:py-2 px-3 sm:h-[44px] font-bold rounded bg-[#00aeff] border-[#00aeff] text-base sm:text-lg text-white">Login</button></Link>
+
+                        <div>
+                            <Link to={'/register'}> <button className="py-1 text-xl sm:py-2 px-3 sm:h-[44px] font-bold rounded bg-[#b18b5e] btn  sm:text-lg text-white">Registration</button></Link>
+                            <Link to={'/login'}> <button className="py-1  text-lg sm:py-2 px-3 sm:h-[44px] font-bold rounded bg-[#b18b5e] btn  sm:text-lg text-white">Login</button></Link>
+                        </div>
                     }
                 </div>
-
-                {/* <div className="navbar-end">
-                    <Link to={'/login'} className="btn bg-[#a55e3f bg-[#b18b5e] bg text-white ">  Login</Link>
-                </div> */}
             </div>
         </div>
     );
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,11 +3,13 @@ import Home from "../Components/Home/Home";
 import Root from "../Layout/Root";
 import Login from "../Components/Login";
 import Register from "../Components/Register";
+import ErrorPage from "../Pages/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root></Root>,
+        errorElement:<ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
