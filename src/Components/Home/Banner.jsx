5d6c6a8bedf319456e5532502d783d19b9565e00
boniaminshawon <shawonboni29@gmail.com>
2024-04-30 01:37:40 +0600

    finish the design of the banner

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -7,7 +7,7 @@ import { Pagination, Autoplay } from 'swiper/modules';
 
 
 const Banner = () => {
-   
+
     return (
         <div className="  
         font-primary">
@@ -24,41 +24,48 @@ const Banner = () => {
             >
 
 
-                {/* handmade arts and crafts according to your needs */}
-                {/* Wooden Hand Craft Elegance in Art */}
-                {/* Designed by artists, made by us, just for you. */}
-                {/* CREATED BY HAND.
-CREATED WITH LOVE.
-Manufactura is clean & responsive WordPress theme exclusively built for wood, leather and other handmade product crafters. */}
-                {/* What we learned so far
-WE BELIEVE THE BEST IDEAS EMERGE FROM THE SIMPLEST TOOLS */}
-                <SwiperSlide><div style={{ backgroundImage: 'url("https://i.ibb.co/cYnPzMW/maxresdefault-4.jpg")' }} className="bg-center bg-cover  bg-no-repeat h-[85vh] flex flex-col justify-center items-center">
-                    <h1 className="text-5xl font-heading font-bold"> CREATED BY HAND ,
-                        CREATED WITH LOVE.</h1>
-                        <p className="my-5">Manufactura is clean & responsive WordPress theme exclusively built for wood, leather and other handmade product crafters.</p>
+                
+                <SwiperSlide><div style={{ backgroundImage: 'url("https://i.ibb.co/cYnPzMW/maxresdefault-4.jpg")' }} className="bg-center bg-cover bg-opacity-100 bg-opacity  bg-no-repeat h-[85vh] flex flex-col justify-center items-center">
+                    <h1 style={{ lineHeight: '4rem' }} className="text-5xl  font-heading font-bold text-center "> We Believe The Best Ideas Emerge <br /> From The Simple Tools.</h1>
+                    <p className="mt-5  text-xl font-medium px-28 text-center font-primary  ">Discover the perfect fusion of rustic charm and timeless elegance on our jute and wooden art and craft website. Explore our handcrafted collection, where each piece tells a story of sustainability, craftsmanship, and natural beauty.</p>
+                    <div className="mb-32 mt-5"><a href="#scroll"><button className="px-3 py-2 text-white text-xl font-heading1 font-semibold rounded bg-[#b18b5e]">Explore More</button></a></div>
                 </div></SwiperSlide>
 
-                <SwiperSlide><div style={{ backgroundImage: 'url("https://i.ibb.co/5TbpHPS/THESYS-Lookbook-07-Hi20-Ryan20and20-Goodman.webp")' }} className="bg-center bg-cover bg-no-repeat h-[85vh]">Best modern wood handmade and crafty art collection</div></SwiperSlide>
+                <SwiperSlide>
+                    <div style={{ backgroundImage: 'url("https://i.ibb.co/5TbpHPS/THESYS-Lookbook-07-Hi20-Ryan20and20-Goodman.webp")' }} className="bg-center bg-cover bg-no-repeat h-[85vh] flex flex-col justify-center items-center ">
+                        <h1 style={{ lineHeight: '4rem' }} className="text-5xl  font-heading font-bold text-center "> Best Modern Wood Handmade And <br /> Crafty Art Collection</h1>
+                        <p className="mt-5  text-xl font-medium px-28 text-center font-primary  ">Discover the perfect fusion of rustic charm and timeless elegance on our jute and wooden art and craft website. Explore our handcrafted collection, where each piece tells a story of sustainability, craftsmanship, and natural beauty.</p>
+                         <div className="mb-32 mt-5"><a href="#scroll"><button className="px-3 py-2 text-white text-xl font-heading1 font-semibold rounded bg-[#b18b5e]">Explore More</button></a></div>
+                    </div>
+                </SwiperSlide>
 
-                <SwiperSlide><div style={{ backgroundImage: 'url("https://i.ibb.co/QnWJHYS/maxresdefault-7.jpg")' }} className="bg-center bg-cover bg-no-repeat h-[85vh]"></div></SwiperSlide>
+                <SwiperSlide>
+                    <div style={{ backgroundImage: 'url("https://i.ibb.co/QnWJHYS/maxresdefault-7.jpg")' }} className="bg-center bg-cover bg-no-repeat h-[85vh] flex flex-col justify-center items-center">
+                        <h1 style={{ lineHeight: '4rem' }} className="text-5xl  font-heading font-bold text-center ">  Create By Hand, Created With Love.</h1>
+                        <p className="mt-5  text-xl font-medium px-28 text-center font-primary  ">Discover the perfect fusion of rustic charm and timeless elegance on our jute and wooden art and craft website. Explore our handcrafted collection, where each piece tells a story of sustainability, craftsmanship, and natural beauty.</p>
+                         <div className="mb-32 mt-5"><a href="#scroll"><button className="px-3 py-2 text-white text-xl font-heading1 font-semibold rounded bg-[#b18b5e]">Explore More</button></a></div>
+                    </div>
+                </SwiperSlide>
 
-                <SwiperSlide><div style={{ backgroundImage: 'url("https://i.ibb.co/q0VyfR3/360-F-677438536-Kd-Qws7lj-Ro6-MRd-Jdq-Bl-WLif-WRn-NTS1-Rt.jpg")' }} className="bg-center bg-cover bg-no-repeat h-[85vh]"></div></SwiperSlide>
+                <SwiperSlide>
+                    <div style={{ backgroundImage: 'url("https://i.ibb.co/q0VyfR3/360-F-677438536-Kd-Qws7lj-Ro6-MRd-Jdq-Bl-WLif-WRn-NTS1-Rt.jpg")' }} className="bg-center bg-cover bg-no-repeat h-[85vh] flex flex-col justify-center items-center">
+                        <h1 style={{ lineHeight: '4rem' }} className="text-5xl  font-heading font-bold text-center text-white ">Wooden Hand Craft Elegance in Art </h1>
+                        <p className="mt-5 text-white text-xl font-medium px-28 text-center font-primary  ">Discover the perfect fusion of rustic charm and timeless elegance on our jute and wooden art and craft website. Explore our handcrafted collection, where each piece tells a story of sustainability, craftsmanship, and natural beauty.</p>
+                         <div className="mb-32 mt-5"><a href="#scroll"><button className="px-3 py-2 text-white text-xl font-heading1 font-semibold rounded bg-[#b18b5e]">Explore More</button></a></div>
+                    </div>
+                </SwiperSlide>
 
+                <SwiperSlide><div style={{ backgroundImage: 'url("https://i.ibb.co/LP9WCYW/handmade-natural-jute-knitted-items-for-home-decoration-scandinavian-style-beige-tones-no-people-sus.jpg")' }} className="bg-center bg-cover bg-opacity-100 bg-opacity  bg-no-repeat h-[85vh] flex flex-col justify-center items-center">
+                    <h1 style={{ lineHeight: '4rem' }} className="text-5xl  font-heading font-bold text-center text-white ">Designed By Artists, Made <br /> By Us, Just For You.</h1>
+                    <p className="mt-5 text-white  text-xl font-medium px-28 text-center font-primary  ">Discover the perfect fusion of rustic charm and timeless elegance on our jute and wooden art and craft website. Explore our handcrafted collection, where each piece tells a story of sustainability, craftsmanship, and natural beauty.</p>
+                     <div className="mb-32 mt-5"><a href="#scroll"><button className="px-3 py-2 text-white text-xl font-heading1 font-semibold rounded bg-[#b18b5e]">Explore More</button></a></div>
+                </div></SwiperSlide>
 
-                <SwiperSlide><div style={{ backgroundImage: 'url("https://i.ibb.co/LP9WCYW/handmade-natural-jute-knitted-items-for-home-decoration-scandinavian-style-beige-tones-no-people-sus.jpg")' }} className="bg-center bg-cover bg-no-repeat h-[85vh]"></div></SwiperSlide>
-
-                <SwiperSlide><div style={{ backgroundImage: 'url("https://i.ibb.co/VxGTmdh/bohemian-style-decorating-ideas.jpg")' }} className="bg-center bg-cover bg-no-repeat h-[85vh]"></div></SwiperSlide>
-
-
-
-                
-
-              
-
-
-
-
-
+                <SwiperSlide><div style={{ backgroundImage: 'url("https://i.ibb.co/VxGTmdh/bohemian-style-decorating-ideas.jpg")' }} className="bg-center bg-cover bg-opacity-100 bg-opacity  bg-no-repeat h-[85vh] flex flex-col justify-center items-center">
+                    <h1 style={{ lineHeight: '4rem' }} className="text-5xl  font-heading font-bold text-center ">  Best Modern Wood Handmade And <br /> Crafty Art Collection.</h1>
+                    <p className="mt-5 text-white text-xl font-medium px-28 text-center font-primary  ">Discover the perfect fusion of rustic charm and timeless elegance on our jute and wooden art and craft website. Explore our handcrafted collection, where each piece tells a story of sustainability, craftsmanship, and natural beauty.</p>
+                     <div className="mb-32 mt-5"><a href="#scroll"><button className="px-3 py-2 text-white text-xl font-heading1 font-semibold rounded bg-[#b18b5e]">Explore More</button></a></div>
+                </div></SwiperSlide>
             </Swiper>
         </div>
     );
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
 
-            <div className="my-16">
+            <div id="scroll" className="my-16">
                 <h1 className="font-heading1 text-center text-4xl font-bold mb-10 ">Designed by artists, made <br /> by us, just for you.</h1>
                 <p className="font-p text-lg text-center my-5 px-20" >Indulge in a world of artistry and craftsmanship with our exquisite collection of jute and wooden handicrafts. From intricately designed jewelry pieces to charming home decor items, each product is a testament to the skilled hands and creative vision of our artisans. Explore our curated selection and discover the perfect blend of natural beauty, sustainability, and timeless elegance in every meticulously crafted piece. Elevate your space and adorn yourself with the unique charm of our jute and wooden creations.</p>
                 <div className="grid  md:grid-cols-2 lg:grid-cols-3 gap-5">
